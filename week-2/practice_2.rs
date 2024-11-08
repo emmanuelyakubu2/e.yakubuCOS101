2ac59faea9af388ac1c52a721f6971805cbd4e2f
Emmanuel Yakubu <emmanuelyakubu317@gmail.com>
2024-11-08 11:34:53 +0100

    (My Second Commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main() {
+	
+	println!(); // prints just a
+
+	println!("hello ");//prints
+
+    println!("format {} arguments", "some"); //prints format some arguments
+
+    println!("My name is {}. I am a student of Software Engineering", "Emmanuel Oghenetejiri Yakubu");
+
+}
